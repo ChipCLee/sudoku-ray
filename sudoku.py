e1d059dab412476d35b54a65afae4a4c682ad58d
Chip Lee <chiplee@gmail.com>
2024-02-21 17:36:49 +0400

    add Row

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,12 @@
 # %load sudoku.py
+import pandas as pd
+import numpy as np
+def getPuzzle(puzale_str):
+    return np.array(list(puzale_str)).reshape(9,9)
+
 def isSolved(np_cells):
     list=np_cells.reshape(81)
-    print(list)
+    logger.debug(list)
     return not ('0' in list)
 
 def map2orginal(row, col, block_row, block_col):
@@ -11,10 +16,6 @@ def getBlockIndex(row_index, col_index):
     r= row_index//3
     c = col_index//3
     return (r,c)
-
-# def getBlock(row, col, np_cells):
-#     r, c = getBlockIndex(row,col)
-#     return np_cells[r*3:r*3+3, c*3:c*3+3]
 
 def getBlock(block_row, block_col, np_cells):
     return np_cells[block_row*3:block_row*3+3, block_col*3:block_col*3+3]
@@ -28,14 +29,12 @@ def getRow(row, np_cells):
 def getCol(col, np_cells):
     return np_cells[:, col]
 
-def calculate(row_index, col_index, np_cells):
-    row_index=row_index
-    col_index=col_index
+def calculate_possibilitie(row_index, col_index, np_cells):
     row=getRow(row_index, np_cells)
     col=getCol(col_index, np_cells)
     block_row, block_col = getBlockIndex(row_index, col_index)
     block=getBlockList(block_row, block_col, np_cells)
-    possibilities={i+1 for i in range(9)}
+    possibilities={i for i in "123456789"}
     for i in row:
         if i in possibilities:
             possibilities.remove(i)
@@ -52,7 +51,7 @@ def calculate_cells(np_cells):
     changed=True
     results=[]
     iterations=0
-    logger.info(np_cells)
+    logger.debug(np_cells)
     while changed:
         iterations+=1
         changed=False
@@ -60,7 +59,7 @@ def calculate_cells(np_cells):
         for i in range(9):
             for j in range(9):
                 if np_cells[i,j] == '0' :
-                    results.append(calculate(i,j,np_cells))
+                    results.append(calculate_possibilitie(i,j,np_cells))
                     solved=False
 
 
@@ -73,9 +72,142 @@ def calculate_cells(np_cells):
         logger.debug("iterations: {}".format(iterations) )
         logger.debug("change: {}".format(changed) )
 
-    logger.info(np_cells)
+    logger.debug(np_cells)
+    return solved
+
+def addCellToMissingsMap(key, cell, missings_map):
+    if key in missings_map.keys():
+        missings_map[key].append(cell)
+    else:
+        missings_map[key]=[cell]
+
+def getMissingValeusAndUnknownCells(block):
+    missing_values={i for i in "123456789"}
+    unknown_cells=[]
+    for i in range(3):
+        for j in range(3):
+            v=block[i,j]
+            if v=='0':
+                unknown_cells.append((i,j))
+            else:
+                missing_values.remove(v)
+    logger.debug("unknown_cells: {}".format(unknown_cells) )
+    logger.debug("missing_values".format(missing_values))
+    return (missing_values, unknown_cells)
+
+class Block:
+    def __init__(self, r, c, np_cells) -> None:
+        self.block=getBlock(r,c,np_cells)
+        logger.debug(self.block)
+        self.block_row, self.block_col=(r,c)
+        self.np_cells=np_cells
+
+        self.missings_map=self.getMissingsMap()
+        
+        logger.debug(self.missings_map)
+
+    def getMissingsMap(self):
+        missing_values, unknown_cells = getMissingValeusAndUnknownCells(self.block)
+        logger.debug("missing_values: {}, unknown_cells: {}".format(missing_values, unknown_cells))
+        
+        missings_map={}
+
+        for key in missing_values:
+            for cell in unknown_cells:
+                logger.debug("key {}, cell: {}".format(key, cell))
+                if self.couldfit(key, cell):
+                    logger.debug("add {} to {}".format(cell, key))
+                    addCellToMissingsMap(key, cell, missings_map)
+                else:
+                    logger.debug("could not add {} to {}".format(cell, key))
+        return missings_map
+
+
+    def couldfit(self, key, cell):
+        row_index, col_index = map2orginal(cell[0], cell[1],self.block_row, self.block_col)
+
+        row=getRow(row_index, self.np_cells)
+       
+        col=getCol(col_index, self.np_cells)
+       
+        result =True
+        if key in row:
+            logger.debug("found {} in {}".format(key, row))
+            result=False
+        else:
+            logger.debug("No {} in row {}: {}".format(key, row_index, row))
+        if key in col:
+            logger.debug("found {} in {}".format(key, col))
+            result=False
+        else:
+            logger.debug("No {} in col {}: {}".format(key,col_index, col))
+       
+        return result
+
+    # def addpos(self, m, u):
+    #     if m in self.missings.keys():
+    #         poss=self.missings[m]
+    #         poss.append(u)
+    #     else:
+    #         poss=[u]
+    #         self.missings[m]=poss
+
+    def removepos(self,removes):
+        deleted=[]
+        for r in removes:
+            for key in self.missings_map.keys():
+                if r in self.missings_map[key]:
+                    self.missings_map[key].remove(r)
+                    if len(self.missings_map[key])==0:
+                        deleted.append(key)
+                    
+        for d in deleted:
+            self.missings_map.pop(d)
+
+
+    def calculate(self):
+        removes=[]
+        changed=False
+        repeat=True
+        while repeat:
+            repeat=False
+            for key in self.missings_map.keys():
+                if len(self.missings_map[key]) == 1:
+                    px, py=self.missings_map[key][0]
+                    logger.debug("Solved {} at {}".format(key,self.missings_map[key][0]))
+                    self.block[px, py]=key
+                    removes.append((px,py))
+                    logger.debug(self.block)
+                    changed=True
+                    repeat=False
+            self.removepos(removes)
+        
+        return changed
+    
+def calculate_blocks(np_cells):
+    changed=False
+    for block_row in range(3):
+        for block_col in range(3):
+            logger.debug("solving {},{}".format(block_row, block_col) )
+            b=Block(block_row, block_col, np_cells)
+            if b.calculate():
+                changed=True
+    return changed
+
+def single_solve(np_cells):
+    changed=True
+    solved=False
+    while changed:
+        changed=False
+        if calculate_cells(np_cells):
+            solved=True
+        else:
+            if calculate_blocks(np_cells):
+                changed=True
+        if not solved:
+            solved=isSolved(np_cells)
+
+    logger.debug(np_cells)
     return solved
 
 logger.debug('info')
-print(isSolved(np_solution))
-print(isSolved(np_cells))
